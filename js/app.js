d8c6d7433bfc5b7f40dfa0e3d2be0c64a4a240af
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-22 15:01:10 +0300

    add scroll animation

--- js/app.js
+++ js/app.js
@@ -2,9 +2,24 @@
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-ScrollSmoother.create({
-    wrapper: '.wrapper',
-    content: '.content',
-    smooth: 1.5,
-    effects: true
-})
+if (ScrollTrigger.isTouch !==1) {
+
+    ScrollSmoother.create({
+        wrapper: '.wrapper',
+        content: '.content',
+        smooth: 1.5,
+        effects: true
+    })
+
+
+	gsap.fromTo('.hero-section', { opacity: 1 }, {
+		opacity: 0,
+		scrollTrigger: {
+			trigger: '.hero-section',
+			start: 'center',
+			end: '820',
+			scrub: true
+		}
+	})
+
+}
